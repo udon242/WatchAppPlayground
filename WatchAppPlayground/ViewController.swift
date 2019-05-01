d7c1a3fc7e8fb9c0797999d7cc05ea806a23b144
udon11 <ylu242@gmail.com>
2019-05-01 11:43:01 +0900

    add Collection View

--- WatchAppPlayground/ViewController.swift
+++ WatchAppPlayground/ViewController.swift
@@ -16,9 +16,25 @@ class ViewController: UIViewController {
     @IBAction func onClickButton(_ sender: Any) {
         self.textFieldValue.text = "Hello"
     }
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .red
+        
+        return cell
     }
 }
